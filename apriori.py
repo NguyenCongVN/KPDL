6f453b79feb99f3bfa0639ab958f1a4179f88bad
NguyenCongVn <cong.nguyen@bootai.com>
2024-03-01 15:09:53 +0700

    add aproori

---
+++ apriori.py
@@ -0,0 +1,20 @@
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
+import pandas as pd
+
+# Sample dataset
+dataset = [['Milk', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'],
+           ['Dill', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'],
+           ['Milk', 'Apple', 'Kidney Beans', 'Eggs'],
+           ['Milk', 'Unicorn', 'Corn', 'Kidney Beans', 'Yogurt'],
+           ['Corn', 'Onion', 'Onion', 'Kidney Beans', 'Ice cream', 'Eggs']]
+
+# Apply one-hot encoding
+te = TransactionEncoder()
+te_ary = te.fit(dataset).transform(dataset)
+df = pd.DataFrame(te_ary, columns=te.columns_)
+
+# Apply Apriori algorithm
+frequent_itemsets = apriori(df, min_support=0.6, use_colnames=True)
+
+print(frequent_itemsets)
